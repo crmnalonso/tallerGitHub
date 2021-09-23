d501229fb9ba6acfbea69d46f4c3159587cf2c42
Carmen Alonso <carmenalonso@MBP-de-Carmen.home>
2021-09-23 17:35:48 +0200

    Reto 2: Proyecto Maven

---
+++ taller-maven/src/main/java/com/everis/bootcamp/App.java
@@ -0,0 +1,18 @@
+package com.everis.bootcamp;
+
+import org.joda.time.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        DateTime dt = DateTime.now();
+        System.out.println(dt);
+    }
+}
